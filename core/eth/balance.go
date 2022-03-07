ff17769ec436d958a6929bb79f60b15391126d13
GG <zhanggzgzhz@gmail.com>
2022-03-07 16:27:45 +0800

    feat: change balance from int64 to string

--- core/eth/balance.go
+++ core/eth/balance.go
@@ -6,12 +6,12 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-func (e *EthChain) Balance(address string) (int64, error) {
+func (e *EthChain) Balance(address string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), e.timeout)
 	defer cancel()
 	result, err := e.RemoteRpcClient.BalanceAt(ctx, common.HexToAddress(address), nil)
 	if err != nil {
-		return 0, err
+		return "0", err
 	}
-	return result.Int64(), nil
+	return result.String(), nil
 }
