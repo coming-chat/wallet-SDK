62a4d5b72025af28bd1e54c56db5681140409866
qinghuan-chain <1245816264@qq.com>
2022-03-08 14:29:33 +0800

    feat: add notes

--- core/eth/balance.go
+++ core/eth/balance.go
@@ -6,6 +6,11 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// @title    主网代币余额查询
+// @description   返回主网代币余额，decimal为代币精度
+// @auth      清欢
+// @param     (walletAddress)     (string)  合约名称，钱包地址
+// @return    (string,error)       代币余额，错误信息
 func (e *EthChain) Balance(address string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), e.timeout)
 	defer cancel()
