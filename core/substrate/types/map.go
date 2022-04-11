c1a51dc5ad077204de6d8aea1757b52119621696
Gkirito <lownkid@outlook.com>
2022-04-11 17:07:52 +0800

    🌟 feat(core/substrate): add customscale

---
+++ core/substrate/types/map.go
@@ -0,0 +1,55 @@
+package types
+
+import (
+	"errors"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/scale"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+)
+
+var (
+	k, v *types.Si1Type
+)
+
+type Map struct {
+	data map[interface{}]interface{}
+}
+
+func NewMap(kd, vd *types.Si1Type) Map {
+	k = kd
+	v = vd
+	return Map{}
+}
+
+func (m *Map) Decode(decoder scale.Decoder) error {
+	if k == nil || v == nil {
+		return errors.New("please new map")
+	}
+	m.data = map[interface{}]interface{}{}
+	length, err := decoder.ReadOneByte()
+	if err != nil {
+		return err
+	}
+	length = length >> 2
+	if !k.Def.IsVariant {
+		return errors.New("not supported type")
+	}
+	for i := 0; uint8(i) < length; i++ {
+		kIndex, err := decoder.ReadOneByte()
+		if err != nil {
+			return err
+		}
+		for _, variant := range k.Def.Variant.Variants {
+			if kIndex != byte(variant.Index) {
+				continue
+			}
+			var v types.U128
+			err = decoder.Decode(&v)
+			if err != nil {
+				return err
+			}
+			m.data[string(variant.Name)] = v
+			break
+		}
+	}
+	return nil
+}
---
+++ core/substrate/types/map_test.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"encoding/hex"
+	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/itering/subscan/util/base58"
+	"github.com/itering/subscan/util/ss58"
+	"testing"
+)
+
+func TestGetXAssertsBalance(t *testing.T) {
+	api, err := gsrpc.NewSubstrateAPI("wss://testnet3.chainx.org")
+	if err != nil {
+	}
+
+	metadata, err := api.RPC.State.GetMetadataLatest()
+	if err != nil {
+		return
+	}
+
+	ss58Format := base58.Decode("5QUEnWNMDFqsbUGpvvtgWGUgiiojnEpLf7581ELLAQyQ1xnT")
+	publicKey, err := hex.DecodeString(ss58.Decode("5QUEnWNMDFqsbUGpvvtgWGUgiiojnEpLf7581ELLAQyQ1xnT", int(ss58Format[0])))
+	if err != nil {
+		return
+	}
+
+	assetId, err := types.EncodeToBytes(uint32(1))
+	if err != nil {
+		return
+	}
+
+	call, err := types.CreateStorageKey(metadata, "XAssets", "AssetBalance", publicKey, assetId)
+	if err != nil {
+		return
+	}
+	entryMetadata, err := metadata.FindStorageEntryMetadata("XAssets", "AssetBalance")
+	if err != nil {
+		return
+	}
+	i := entryMetadata.(types.StorageEntryMetadataV14).Type.AsMap.Value
+	kIndex := metadata.AsMetadataV14.EfficientLookup[i.Int64()].Params[0].Type.Int64()
+	vValue := metadata.AsMetadataV14.EfficientLookup[i.Int64()].Params[1].Type.Int64()
+	data := NewMap(metadata.AsMetadataV14.EfficientLookup[kIndex], metadata.AsMetadataV14.EfficientLookup[vValue])
+	//var res string
+	//err = client.CallWithBlockHash(api.Client, &res, "state_getStorage", nil, call.Hex())
+	//if err != nil {
+	//	return
+	//}
+	ok, err := api.RPC.State.GetStorageLatest(call, &data)
+	if err != nil {
+		t.Log(err)
+	}
+	t.Log(ok)
+	t.Log(data)
+}
