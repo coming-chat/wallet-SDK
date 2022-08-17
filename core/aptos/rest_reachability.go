893e1b2cdb9f4cf13c4a8755f51a67c0a73bda58
GG <zhanggzgzhz@gmail.com>
2022-08-17 11:45:31 +0800

    feat: aptos support rest reachability.

---
+++ core/aptos/rest_reachability.go
@@ -0,0 +1,50 @@
+package aptos
+
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+
+	"github.com/coming-chat/wallet-SDK/core/base"
+	"github.com/coming-chat/wallet-SDK/pkg/httpUtil"
+)
+
+type RestReachability struct {
+}
+
+func NewRestReachability() *RestReachability {
+	return &RestReachability{}
+}
+
+// @return latency (ms) of rpc query blockNumber. -1 means the connection failed.
+func (r *RestReachability) LatencyOf(rpc string, timeout int64) (l *base.RpcLatency, err error) {
+	l = &base.RpcLatency{
+		RpcUrl:  rpc,
+		Latency: -1,
+		Height:  -1,
+	}
+
+	timeStart := time.Now() // Time Start
+	body, err := httpUtil.Get(rpc, nil)
+	if err != nil {
+		return l, err
+	}
+
+	model := struct {
+		LedgerVersion string `json:"ledger_version"`
+		// BlockHeight   string `json:"block_height"`
+	}{}
+	err = json.Unmarshal(body, &model)
+	if err != nil {
+		return l, err
+	}
+	heightInt, err := strconv.ParseInt(model.LedgerVersion, 10, 64)
+	if err != nil {
+		heightInt = 0
+	}
+	timeCost := time.Since(timeStart) // Time End
+
+	l.Height = heightInt
+	l.Latency = timeCost.Milliseconds()
+	return l, nil
+}
---
+++ core/aptos/rest_reachability_test.go
@@ -0,0 +1,42 @@
+package aptos
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/coming-chat/wallet-SDK/core/base"
+)
+
+type dddd struct {
+}
+
+func (d *dddd) ReachabilityDidReceiveNode(tester *base.ReachMonitor, latency *base.RpcLatency) {
+	fmt.Printf(".... delegate did receive height %v, latency %v, node %v\n", latency.Height, latency.Latency, latency.RpcUrl)
+}
+
+func (d *dddd) ReachabilityDidFailNode(tester *base.ReachMonitor, latency *base.RpcLatency) {
+	fmt.Printf(".... delegate did fail height %v, latency %v, node %v\n", latency.Height, latency.Latency, latency.RpcUrl)
+	// tester.StopConnectivity()
+}
+
+func (d *dddd) ReachabilityDidFinish(tester *base.ReachMonitor, overview string) {
+	fmt.Printf(".... delegate did finish %v\n", overview)
+}
+
+func TestRpcReachability_Test(t *testing.T) {
+	reach := NewRestReachability()
+	monitor := base.NewReachMonitorWithReachability(reach)
+	monitor.ReachCount = 3
+	monitor.Delay = 3000
+	monitor.Timeout = 1500
+	t.Log(reach)
+
+	rpcUrls := []string{testnetRestUrl}
+	rpcListString := strings.Join(rpcUrls, ",")
+	// res := reach.StartConnectivitySync(rpcListString)
+	// t.Log(res)
+
+	delegate := &dddd{}
+	monitor.StartConnectivityDelegate(rpcListString, delegate)
+}
