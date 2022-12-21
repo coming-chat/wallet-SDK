3a4f8dff49fc2c0c5c41d1e3fd37f4c1e015f77a
Gkirito <lownkid@outlook.com>
2022-12-21 10:36:58 +0800

    🧪 test(core/sui): add new token & token balance test

--- core/sui/token_test.go
+++ core/sui/token_test.go
@@ -7,11 +7,24 @@ import (
 )
 
 func TestBalance(t *testing.T) {
-	address := "0xbb8f7e72ae99d371020a1ccfe703bfb64a8a430f"
+	address := "0x2ecb102385afd954bf06f2a3a4ac648eb7a536e0"
 
 	chain := DevnetChain()
 	b, err := chain.BalanceOfAddress(address)
 	assert.Nil(t, err)
 
 	t.Log(b)
+}
+
+func TestTokenBalance(t *testing.T) {
+	chain := DevnetChain()
+	token, err := NewToken(chain, "0x2d79a3c70aa3f3a3feabbf54b7b520f956c4ef8d::AAA::AAA")
+	if err != nil {
+		t.Fatal(err)
+	}
+	balance, err := token.BalanceOfAddress("0x2ecb102385afd954bf06f2a3a4ac648eb7a536e0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(balance)
 }
