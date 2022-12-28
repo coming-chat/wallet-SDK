68b77d86a0bf1caa1f1efd56e5ed033782196254
Gkirito <lownkid@outlook.com>
2022-12-28 10:49:00 +0800

    🌟 feat(core/sui): add move call func

---
+++ core/sui/move_call.go
@@ -0,0 +1,49 @@
+package sui
+
+import (
+	"context"
+	"github.com/coming-chat/go-sui/types"
+)
+
+const gasBudget = 1000
+
+func (c *Chain) BaseMoveCall(address, packageId, module, funcName string, arg []any) (*Transaction, error) {
+	client, err := c.client()
+	if err != nil {
+		return nil, err
+	}
+	addr, err := types.NewAddressFromHex(address)
+	if err != nil {
+		return nil, err
+	}
+	packageIdHex, err := types.NewHexData(packageId)
+	if err != nil {
+		return nil, err
+	}
+	suiToken := NewTokenMain(c)
+	coins, err := suiToken.getCoins(address)
+	if err != nil {
+		return nil, err
+	}
+	coin, err := coins.PickCoinNoLess(gasBudget)
+	if err != nil {
+		return nil, err
+	}
+	tx, err := client.MoveCall(
+		context.Background(),
+		*addr,
+		*packageIdHex,
+		module,
+		funcName,
+		[]string{},
+		arg,
+		&coin.Reference.ObjectId,
+		gasBudget,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Transaction{
+		Txn: *tx,
+	}, nil
+}
