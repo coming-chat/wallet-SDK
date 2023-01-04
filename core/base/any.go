90f135d9e173bbf1149c7b78841cdacaa4e038e5
Gkirito <lownkid@outlook.com>
2023-01-04 10:48:00 +0800

    🔨 refactor(core/base/any): change uint type

--- core/base/any.go
+++ core/base/any.go
@@ -1,6 +1,9 @@
 package base
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"math/big"
+)
 
 // exchange `Aniable Object` & `Any`
 type Aniable interface {
@@ -26,30 +29,28 @@ func (a *Any) SetInt32(v int32)     { a.Value = v }
 func (a *Any) SetInt64(v int64)     { a.Value = v }
 func (a *Any) SetInt128(v *BigInt)  { a.Value = v }
 func (a *Any) SetInt256(v *BigInt)  { a.Value = v }
+func (a *Any) SetUInt8(v *BigInt)   { n := uint8(v.bigint.Uint64()); a.Value = &n }
+func (a *Any) SetUInt16(v *BigInt)  { n := uint16(v.bigint.Uint64()); a.Value = &n }
+func (a *Any) SetUInt32(v *BigInt)  { n := uint32(v.bigint.Uint64()); a.Value = &n }
+func (a *Any) SetUInt64(v *BigInt)  { n := v.bigint.Uint64(); a.Value = &n }
 func (a *Any) SetUint128(v *BigInt) { a.Value = v }
 func (a *Any) SetUint256(v *BigInt) { a.Value = v }
 
-// func (a *Any) SetUInt8(v uint8)     { a.Value = v }
-// func (a *Any) SetUInt16(v uint16)   { a.Value = v }
-// func (a *Any) SetUInt32(v uint32)   { a.Value = v }
-// func (a *Any) SetUInt64(v uint64)   { a.Value = v }
-
-func (a *Any) GetString() string  { return a.Value.(string) }
-func (a *Any) GetBool() bool      { return a.Value.(bool) }
-func (a *Any) GetInt() int        { return a.Value.(int) }
-func (a *Any) GetInt8() int8      { return a.Value.(int8) }
-func (a *Any) GetInt16() int16    { return a.Value.(int16) }
-func (a *Any) GetInt32() int32    { return a.Value.(int32) }
-func (a *Any) GetInt64() int64    { return a.Value.(int64) }
-func (a *Any) GetInt128() string  { return a.Value.(*BigInt).GetString(128) }
-func (a *Any) GetInt256() string  { return a.Value.(*BigInt).GetString(256) }
-func (a *Any) GetUint128() string { return a.Value.(*BigInt).GetString(128) }
-func (a *Any) GetUint256() string { return a.Value.(*BigInt).GetString(256) }
-
-// func (a *Any) GetUInt8() uint8    { return a.Value.(uint8) }
-// func (a *Any) GetUInt16() uint16  { return a.Value.(uint16) }
-// func (a *Any) GetUInt32() uint32  { return a.Value.(uint32) }
-// func (a *Any) GetUInt64() uint64  { return a.Value.(uint64) }
+func (a *Any) GetString() string   { return a.Value.(string) }
+func (a *Any) GetBool() bool       { return a.Value.(bool) }
+func (a *Any) GetInt() int         { return a.Value.(int) }
+func (a *Any) GetInt8() int8       { return a.Value.(int8) }
+func (a *Any) GetInt16() int16     { return a.Value.(int16) }
+func (a *Any) GetInt32() int32     { return a.Value.(int32) }
+func (a *Any) GetInt64() int64     { return a.Value.(int64) }
+func (a *Any) GetInt128() *BigInt  { return a.Value.(*BigInt) }
+func (a *Any) GetInt256() *BigInt  { return a.Value.(*BigInt) }
+func (a *Any) GetUInt8() *BigInt   { return &BigInt{new(big.Int).SetUint64(uint64(*a.Value.(*uint8)))} }
+func (a *Any) GetUInt16() *BigInt  { return &BigInt{new(big.Int).SetUint64(uint64(*a.Value.(*uint16)))} }
+func (a *Any) GetUInt32() *BigInt  { return &BigInt{new(big.Int).SetUint64(uint64(*a.Value.(*uint32)))} }
+func (a *Any) GetUInt64() *BigInt  { return &BigInt{new(big.Int).SetUint64(*a.Value.(*uint64))} }
+func (a *Any) GetUint128() *BigInt { return a.Value.(*BigInt) }
+func (a *Any) GetUint256() *BigInt { return a.Value.(*BigInt) }
 
 type AnyArray struct {
 	Values []any
