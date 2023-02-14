ebdfb72e199384cfe61d42b7eb48f38477664ad0
GG <863867759@qq.com>
2023-02-14 14:33:47 +0800

    feat: sui stake api.

--- core/sui/chain_stake.go
+++ core/sui/chain_stake.go
@@ -2,21 +2,67 @@ package sui
 
 import (
 	"context"
+	"encoding/json"
+	"math/big"
 	"strconv"
+
+	"github.com/coming-chat/go-sui/types"
+	"github.com/coming-chat/wallet-SDK/core/base"
 )
 
-type Validator struct {
-	Address    string
-	Name       string
-	Desc       string
-	ImageUrl   string
-	ProjectUrl string
-	APY        float64
-	StakedSui  string
-	Epoch      int64
+var cachedSuiSystemState *types.SuiSystemState
+
+type ValidatorState struct {
+	// The current epoch in Sui. An epoch takes approximately 24 hours and runs in checkpoints.
+	Epoch      int64        `json:"epoch"`
+	Validators []*Validator `json:"validators"`
+
+	// The amount of all tokens staked in the Sui Network.
+	TotalStaked string `json:"totalStaked"`
+	// The amount of rewards won by all Sui validators in the last epoch.
+	TotalRewards string `json:"lastEpochReward"`
+
+	Time int64 `json:"time"`
 }
 
-func (c *Chain) GetStakeList() ([]*Validator, error) {
+type Validator struct {
+	Address    string  `json:"address"`
+	Name       string  `json:"name"`
+	Desc       string  `json:"desc"`
+	ImageUrl   string  `json:"imageUrl"`
+	ProjectUrl string  `json:"projectUrl"`
+	APY        float64 `json:"apy"`
+
+	Commission      int64  `json:"commission"`
+	TotalStaked     string `json:"totalStaked"`
+	DelegatedStaked string `json:"delegatedStaked"`
+	SelfStaked      string `json:"selfStaked"`
+	TotalRewards    string `json:"totalRewards"`
+	GasPrice        string `json:"gasPrice"`
+}
+
+type DelegationStatus = base.SDKEnumInt
+
+const (
+	DelegationStatusPending DelegationStatus = 0
+	DelegationStatusActived DelegationStatus = 1
+)
+
+type DelegatedStake struct {
+	StakeId          string `json:"stakeId"`
+	ValidatorAddress string `json:"validatorAddress"`
+	Principal        string `json:"principal"`
+	RequestEpoch     int64  `json:"requestEpoch"`
+
+	Status       DelegationStatus `json:"status"`
+	DelegationId string           `json:"delegationId"`
+	EarnedAmount string           `json:"earnedAmount"`
+	Validator    *Validator       `json:"validator"`
+}
+
+func (c *Chain) GetValidatorState() (s *ValidatorState, err error) {
+	defer base.CatchPanicAndMapToBasicError(&err)
+
 	cli, err := c.client()
 	if err != nil {
 		return nil, err
@@ -25,23 +71,181 @@ func (c *Chain) GetStakeList() ([]*Validator, error) {
 	if err != nil {
 		return nil, err
 	}
+	cachedSuiSystemState = state // cache
 
+	totalRewards := big.NewInt(0)
 	var validators = []*Validator{}
 	for _, v := range state.Validators.ActiveValidators {
-		meta := v.Metadata
-
-		validator := Validator{
-			Address:    meta.SuiAddress.String(),
-			Name:       string(meta.Name),
-			Desc:       string(meta.Description),
-			ImageUrl:   string(meta.ImageUrl),
-			ProjectUrl: string(meta.ProjectUrl),
-			APY:        v.CalculateAPY(state.Epoch),
-			StakedSui:  strconv.FormatUint(v.DelegationStakingPool.SuiBalance, 10),
-			Epoch:      int64(state.Epoch),
-		}
-		validators = append(validators, &validator)
+		validator := mapRawValidator(&v, state.Epoch)
+		validators = append(validators, validator)
+		reward := big.NewInt(int64(v.DelegationStakingPool.RewardsPool.Value))
+		totalRewards.Add(totalRewards, reward)
 	}
 
-	return validators, nil
+	delegationStake := big.NewInt(int64(state.Validators.DelegationStake))
+	validatorStake := big.NewInt(int64(state.Validators.ValidatorStake))
+	totalStake := big.NewInt(0).Add(delegationStake, validatorStake)
+
+	res := &ValidatorState{
+		Epoch:      int64(state.Epoch),
+		Validators: validators,
+
+		TotalStaked:  totalStake.String(),
+		TotalRewards: totalRewards.String(),
+	}
+
+	return res, nil
+}
+
+func (c *Chain) GetDelegatedStakes(owner string) (arr []*DelegatedStake, err error) {
+	defer base.CatchPanicAndMapToBasicError(&err)
+
+	addr, err := types.NewAddressFromHex(owner)
+	if err != nil {
+		return nil, err
+	}
+	cli, err := c.client()
+	if err != nil {
+		return nil, err
+	}
+	list, err := cli.GetDelegatedStakes(context.Background(), *addr)
+	if err != nil {
+		return nil, err
+	}
+	if cachedSuiSystemState == nil {
+		cachedSuiSystemState, _ = cli.GetSuiSystemState(context.Background())
+	}
+
+	var stakes = []*DelegatedStake{}
+	for _, s := range list {
+		stake := mapRawStake(&s)
+		stakes = append(stakes, stake)
+	}
+
+	return stakes, nil
+}
+
+func (c *Chain) AddDelegation(owner, amount string, validatorAddress string) (txn *Transaction, err error) {
+	defer base.CatchPanicAndMapToBasicError(&err)
+
+	signer, err := types.NewAddressFromHex(owner)
+	if err != nil {
+		return
+	}
+	validator, err := types.NewAddressFromHex(validatorAddress)
+	if err != nil {
+		return
+	}
+	amountInt, err := strconv.ParseUint(amount, 10, 64)
+	if err != nil {
+		return
+	}
+	cli, err := c.client()
+	if err != nil {
+		return
+	}
+	coins := []types.ObjectId{} // TODO:
+	gasCoin := types.ObjectId{} // TODO:
+	gasBudget := uint64(3000)   // TODO:
+	txBytes, err := cli.RequestAddDelegation(context.Background(), *signer, coins, amountInt, *validator, gasCoin, gasBudget)
+	if err != nil {
+		return
+	}
+	return &Transaction{
+		Txn:          *txBytes,
+		MaxGasBudget: int64(gasBudget),
+	}, nil
+}
+
+func (c *Chain) WithdrawDelegation(owner, delegationId, stakeId string) (txn *Transaction, err error) {
+	defer base.CatchPanicAndMapToBasicError(&err)
+
+	signer, err := types.NewAddressFromHex(owner)
+	if err != nil {
+		return
+	}
+	delegation, err := types.NewHexData(delegationId)
+	if err != nil {
+		return
+	}
+	stakeSui, err := types.NewHexData(stakeId)
+	if err != nil {
+		return
+	}
+	cli, err := c.client()
+	if err != nil {
+		return
+	}
+	gasCoin := types.ObjectId{} // TODO:
+	gasBudget := uint64(3000)   // TODO:
+	txnBytes, err := cli.RequestWithdrawDelegation(context.Background(), *signer, *delegation, *stakeSui, gasCoin, gasBudget)
+	if err != nil {
+		return
+	}
+
+	return &Transaction{
+		Txn:          *txnBytes,
+		MaxGasBudget: int64(gasBudget),
+	}, nil
+}
+
+func mapRawValidator(v *types.Validator, epoch uint64) *Validator {
+	if v == nil {
+		return nil
+	}
+	meta := v.Metadata
+	selfStaked := v.StakeAmount
+	delegatedStaked := v.DelegationStakingPool.SuiBalance
+	totalStaked := selfStaked + delegatedStaked
+	rewardsPoolBalance := v.DelegationStakingPool.RewardsPool.Value
+
+	validator := Validator{
+		Address:    meta.SuiAddress.String(),
+		Name:       string(meta.Name),
+		Desc:       string(meta.Description),
+		ImageUrl:   string(meta.ImageUrl),
+		ProjectUrl: string(meta.ProjectUrl),
+		APY:        v.CalculateAPY(epoch),
+
+		Commission:      int64(v.CommissionRate),
+		SelfStaked:      strconv.FormatUint(selfStaked, 10),
+		DelegatedStaked: strconv.FormatUint(delegatedStaked, 10),
+		TotalStaked:     strconv.FormatUint(totalStaked, 10),
+		TotalRewards:    strconv.FormatUint(rewardsPoolBalance, 10),
+	}
+	return &validator
+}
+
+func mapRawStake(s *types.DelegatedStake) *DelegatedStake {
+	if s == nil {
+		return nil
+	}
+	stake := &DelegatedStake{
+		StakeId:          s.StakedSui.Id.Id.String(),
+		ValidatorAddress: s.StakedSui.ValidatorAddress.String(),
+		Principal:        strconv.FormatUint(s.StakedSui.Principal.Value, 10),
+		RequestEpoch:     int64(s.StakedSui.DelegationRequestEpoch),
+	}
+
+	if cachedSuiSystemState != nil {
+		earned, validator := s.CalculateEarnAmount(cachedSuiSystemState.Validators.ActiveValidators)
+		stake.EarnedAmount = strconv.FormatUint(earned, 10)
+		stake.Validator = mapRawValidator(validator, cachedSuiSystemState.Epoch)
+	}
+
+	statusData, err := json.Marshal(s.DelegationStatus)
+	if err != nil {
+		stake.Status = DelegationStatusPending
+		return stake
+	}
+	var activeStatus types.ActiveDelegationStatus
+	err = json.Unmarshal(statusData, &activeStatus)
+	if err != nil {
+		stake.Status = DelegationStatusPending
+		return stake
+	}
+
+	stake.Status = DelegationStatusActived
+	stake.DelegationId = activeStatus.Active.Id.Id.String()
+	return stake
 }
--- core/sui/chain_stake_test.go
+++ core/sui/chain_stake_test.go
@@ -6,12 +6,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetStakeList(t *testing.T) {
+func TestGetValidatorState(t *testing.T) {
 	chain := TestnetChain()
 
-	list, err := chain.GetStakeList()
+	state, err := chain.GetValidatorState()
+	require.Nil(t, err)
+	for _, v := range state.Validators {
+		t.Logf("%-10v, APY = %v", v.Name, v.APY)
+	}
+}
+
+func TestGetDelegatedStakes(t *testing.T) {
+	chain := TestnetChain()
+	acc := M1Account(t)
+
+	list, err := chain.GetDelegatedStakes(acc.Address())
 	require.Nil(t, err)
 	for _, v := range list {
-		t.Logf("%v, APY = %v", v.Name, v.APY)
+		t.Log(v)
 	}
 }
