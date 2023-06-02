24dd6ae2ac8c10d8c12ad202f0f3dfcf26d9cd67
Gkirito <lownkid@outlook.com>
2023-06-02 20:55:40 +0800

    feat(core/btc): new func for psbt to msgTx

---
+++ core/btc/transaction.go
@@ -0,0 +1,28 @@
+package btc
+
+import (
+	"bytes"
+	"encoding/hex"
+	"github.com/btcsuite/btcd/btcutil/psbt"
+	"github.com/btcsuite/btcd/wire"
+)
+
+func ExtractPSBTHexToMsgTx(psbtTxHex string) (*wire.MsgTx, error) {
+	psbtTx, err := hex.DecodeString(psbtTxHex)
+	if err != nil {
+		return nil, err
+	}
+	return ExtractPSBTPacketToMsgTx(psbtTx, false)
+}
+
+func ExtractPSBTBse64ToMsgTx(psbtTxBase64 string) (*wire.MsgTx, error) {
+	return ExtractPSBTPacketToMsgTx([]byte(psbtTxBase64), true)
+}
+
+func ExtractPSBTPacketToMsgTx(psbtTx []byte, isBase64 bool) (*wire.MsgTx, error) {
+	psbtPacket, err := psbt.NewFromRawBytes(bytes.NewReader(psbtTx), isBase64)
+	if err != nil {
+		return nil, err
+	}
+	return psbt.Extract(psbtPacket)
+}
