2f8b365e1294b7458bfc8bbdf691465ce3eed566
GG <863867759@qq.com>
2024-01-30 16:16:45 +0800

    feat: add donut token transfer.

--- core/eth/donut.go
+++ core/eth/donut.go
@@ -3,10 +3,12 @@ package eth
 import (
 	"encoding/json"
 	"fmt"
+	"math/big"
 
 	"github.com/coming-chat/wallet-SDK/core/base"
 	"github.com/coming-chat/wallet-SDK/core/base/inter"
 	"github.com/coming-chat/wallet-SDK/graphql"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 type donutGraphResp struct {
@@ -69,4 +71,60 @@ func FetchDonutInscriptions(owner string, graphURL string) (arr *DonutInscriptio
 		inscriptions[idx] = node.Node
 	}
 	return &DonutInscriptionArray{AnyArray: inscriptions}, nil
+}
+
+func (c *Chain) BuildDonutTransfer(sender, receiver, tick, amount string) (*Transaction, error) {
+	return c.BuildSrc20Transfer(sender, receiver, "0xf414dF7d8260A8e1e007F72892Cf5F0A7955cf04", tick, amount)
+}
+
+func (c *Chain) BuildSrc20Transfer(sender, receiver string, src20ContractAddress string, tick string, amount string) (txn *Transaction, err error) {
+	defer base.CatchPanicAndMapToBasicError(&err)
+
+	if !common.IsHexAddress(sender) || !common.IsHexAddress(receiver) ||
+		!common.IsHexAddress(src20ContractAddress) {
+		return nil, base.ErrInvalidAddress
+	}
+	amountInt, ok := big.NewInt(0).SetString(amount, 10)
+	if !ok {
+		return nil, base.ErrInvalidAmount
+	}
+	data, err := encodeSrc20OpData(common.HexToAddress(receiver), "transfer", tick, amountInt)
+	if err != nil {
+		return nil, err
+	}
+	gasPrice, err := c.SuggestGasPrice()
+	if err != nil {
+		return nil, err
+	}
+
+	msg := NewCallMsg()
+	msg.SetFrom(sender)
+	msg.SetTo(src20ContractAddress)
+	msg.SetValue("0")
+	msg.SetGasPrice(gasPrice.Value)
+	msg.SetData(data)
+
+	gasLimit, err := c.EstimateGasLimit(msg)
+	if err != nil {
+		return nil, err
+	}
+	msg.SetGasLimit(gasLimit.Value)
+
+	return msg.TransferToTransaction(), nil
+}
+
+func encodeSrc20OpData(receiver common.Address, op, tick string, amount *big.Int) ([]byte, error) {
+	return AbiCoderEncode([]string{"address", "address", "string", "string", "string", "uint256", "uint256", "uint256", "uint256", "uint16", "string"},
+		common.HexToAddress("0x0"),
+		receiver,
+		"src-20",
+		op,
+		tick,
+		big.NewInt(0),
+		big.NewInt(0),
+		big.NewInt(0),
+		amount,
+		uint16(0),
+		"{}",
+	)
 }
