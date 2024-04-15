2835c1899cbc807128c11b753cee38a4f5d6ec24
Gkirito <lownkid@outlook.com>
2024-04-15 11:33:03 +0800

    feat(core/btc): add btc account derviate

---
+++ core/btc/account_derivation.go
@@ -0,0 +1,32 @@
+package btc
+
+import (
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcutil/hdkeychain"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/tyler-smith/go-bip39"
+)
+
+func Derivation(mnemonic string, path string) (*btcec.PrivateKey, error) {
+	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, "")
+	if err != nil {
+		return nil, err
+	}
+	masterKey, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		return nil, err
+	}
+	dPath, err := accounts.ParseDerivationPath(path)
+	if err != nil {
+		return nil, err
+	}
+	key := masterKey
+	for _, n := range dPath {
+		key, err = key.Derive(n)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return key.ECPrivKey()
+}
